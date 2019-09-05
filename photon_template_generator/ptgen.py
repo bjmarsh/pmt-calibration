25ab8a7aeafc657b84451e434a6a71f1428c7ded
Bennett Marsh <bmarsh@physics.ucsb.edu>
2019-09-05 11:44:01 -0700

    adding new SPE templates and area distributions

--- photon_template_generator/ptgen.py
+++ photon_template_generator/ptgen.py
@@ -1,18 +1,29 @@
 import ROOT as r
 import numpy as np
-import cPickle as pickle
 
 model_defs = {
     "r878": {
-        "template": "assets/template_peak_70_75_2GHz.pkl",
-        "input_rate": 2.5,
+        "template" : "assets/templates_r878.root",
+        "areas" : "assets/r878_areas_1450V.root",
+        "input_rates" : [0.7, 1.0, 1.6, 2.0, 5.0],
+        },
+    "r7725": {
+        "template" : "assets/templates_r7725.root",
+        "areas" : "assets/r7725_areas_1400V.root",
+        "input_rates" : [0.7, 1.0, 1.6, 2.0, 5.0],
+        },
+    "et": {
+        "template" : "assets/templates_et.root",
+        "areas" : "assets/et_areas_1700V.root",
+        "input_rates" : [0.7, 1.0, 1.6, 2.0, 5.0],
         }
     }
 
-def GenerateSignal(model="r878", output_samp_freq=2.0, noise=None, extend=None, verbose=False):
+def GenerateSignal(model="r878", samp_freq=2.0, noise=None, extend=None):
     # model defines which PMT to use (see model_defs above)
-    # output_samp_freq is the sampling frequency of the ouput waveform
+    # samp_freq is the sampling frequency of the ouput waveform
     # (in GHz, so 2.0 means time bin spacing of 0.5 ns)
+    # samp_freq must be in the list of input rates from the model defs
     # if noise is not None, add random gaussian noise of rms 'noise'
     # if extend is not None, extend each side of output waveform by 'extend' ns of zero voltage
 
@@ -22,63 +33,49 @@ def GenerateSignal(model="r878", output_samp_freq=2.0, noise=None, extend=None, 
     if model not in model_defs:
         raise Exception("No model definition for PMT model {0}!".format(model))
 
+    if samp_freq not in model_defs[model]["input_rates"]:
+        raise Exception("Template at sampling frequency {0} GHz does not exist for PMT {1}".format(samp_freq, model))
+
     template_file = model_defs[model]["template"]
-    input_rate = model_defs[model]["input_rate"]
-    dt = 1.0 / input_rate
 
-    # Load template
-    with open(template_file, "rb") as pin:
-        # Get template y-array (millivolts)
-        template_y = pickle.load(pin)
-        # Construct template x-array (ns)
-        template_x = np.arange(0, dt * template_y.size, dt)
+    f = r.TFile(template_file)
+    template = f.Get("template_{0}GHz".format(str(samp_freq).replace(".","p")))
+    template.SetDirectory(0)
+    f.Close()
 
-        
-    # re-sample with new time values
-    new_dt = 1.0 / output_samp_freq
-    times = np.arange(0, template_x[-1] + 1e-9, new_dt)
-    voltages = []
-    curj = 0
-    for i in range(times.size):
-        while curj < template_x.size and template_x[curj] < times[i]:
-            curj += 1
-        if curj==0 or curj==template_x.size:
-            voltages.append(0.0)
-        else:
-            voltages.append( template_y[curj-1] + (template_y[curj] - template_y[curj-1])/(template_x[curj] - template_x[curj-1]) * (times[i] - template_x[curj-1]) )
-    voltages = np.array(voltages)
-        
-    prenorm_area = np.trapz(voltages, times)
+    prenorm_area = template.Integral("width")
 
     # Load areas
-    areas = r.TFile.Open("assets/r878_areas.root")
+    f = r.TFile.Open(model_defs[model]["areas"])
     # SPE area dist
-    spe_areas = (areas.Get("ht")).Clone("spe")
-    for b in range(0, spe_areas.GetSize()):
-        # Kill any negative areas
-        if spe_areas.GetBinContent(b) < 0: spe_areas.SetBinContent(b, 0)
-
-    # Get random area        
+    spe_areas = (f.Get("spe_fit")).Clone("spe")
+    # Get random area
     area = spe_areas.GetRandom()
     # Scale template y-values
-    voltages *= area/prenorm_area
-    if verbose: 
-        postnorm_area = np.trapz(voltages, times)
-        print("SPE Area: {0}, Original Template Area: {1}, Scaled Template Area: {2}".format(area, prenorm_area, postnorm_area))
+    template.Scale(area/prenorm_area)
+    f.Close()
 
+    voltages = np.zeros(template.GetNbinsX())
+    for i in range(voltages.size):
+        voltages[i] = template.GetBinContent(i+1)
+
+    dt = 1.0/samp_freq
     if extend is not None:
-        nsamp = int(extend/new_dt)
-        times = np.arange(0, new_dt*(times.size + 2*nsamp), new_dt)
+        nsamp = int(extend/dt)
         voltages = np.append(voltages, np.zeros(nsamp))
         voltages = np.append(np.zeros(nsamp), voltages)
         
     if noise is not None:
         voltages += np.random.normal(loc=0.0, scale=noise, size=voltages.size)
 
+    times = np.arange(voltages.size)*dt
+
     return times, voltages
 
 if __name__ == "__main__":
-    times, voltages = GenerateSignal(output_samp_freq=2.0, noise=0.38, extend=150, verbose=True)
+    # times, voltages = GenerateSignal(model="r878", samp_freq=2.0, noise=0.38, extend=150)
+    # times, voltages = GenerateSignal(model="r7725", samp_freq=2.0, noise=0.38, extend=150)
+    times, voltages = GenerateSignal(model="et", samp_freq=1.6, noise=0.38, extend=150)
 
     import matplotlib.pyplot as plt
     plt.plot(times, voltages)
